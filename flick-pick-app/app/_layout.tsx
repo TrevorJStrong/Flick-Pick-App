ab76d765b861e28a3a3f0b3d3eb47dee2d1e9198
Trevor Strong <36681787+TrevorJStrong@users.noreply.github.com>
2025-08-30 11:15:52 +0000

    build: android folder generated + one signal import fix

--- flick-pick-app/app/_layout.tsx
+++ flick-pick-app/app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { OneSignal, LogLevel } from 'react-native-onesignal';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Colors } from '@/constants/Colors';
